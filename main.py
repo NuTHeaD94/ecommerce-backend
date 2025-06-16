db66d40216db8a18434004ece66fa05a305a83b7
Gulam Naziruddin Makandar <gulamnazirmakandar@gmail.com>
2025-06-16 22:24:44 +0530

    fix: updated CORS settings to allow frontend requests

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow all origins (for development only, we can restrict it later)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
